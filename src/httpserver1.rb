8e3251fda2c30ea3ad9aa9579940467c48feecf0
Ryotaro MORIWAKI <taro@smkwlab.net>
2025-06-30 12:38:46 +0900

    HTTP1.0に対応したサーバになった

--- src/httpserver1.rb
+++ src/httpserver1.rb
@@ -12,18 +12,53 @@ s0 = TCPServer.open(80)
 loop do
   sock = s0.accept
   Thread.new do
-    while line = sock.gets
-      print line
-      sock.print line
-      if line == "\r\n"
-        break
+    line = sock.gets&.chomp
+    if line.nil? || line.empty?
+      sock.close
+      next
+    end
+    _cmd,path,_ver = line.split
+
+    #print path
+    case path
+    when "/hello"
+      sock.puts "HTTP/1.0 200 OK"
+      sock.puts
+      sock.puts "Hello, World!"
+    when "/93"
+      sock.puts "HTTP/1.0 301 Moved Permanently"
+      sock.puts "Location: http://www.kyusan-u.ac.jp"
+      sock.puts
+    else
+      path = path.gsub("/", "")
+      #path = "./#{path}"
+      if File.exist?(path)
+        sock.puts "HTTP/1.0 200 OK"
+        sock.puts "Content-Type: text/plain; charset=UTF-8"
+        sock.puts
+        
+        pp path
+        File.open path,"r" do |f|
+          cnt = 0
+          while line_file = f.gets&.chomp
+            #print cnt += 1 ,": ",line,"\n"
+            sock.puts "#{cnt += 1}:#{line_file}"
+          end 
+        end
+      else
+        sock.puts "HTTP/1.0 404 Not Found"
+        sock.puts "Content-Type: text/plain; charset=UTF-8"
+        sock.puts
+        #sock.puts "File not found: #{path}"
       end
+      #sock.puts path
     end
     sock.close
-    sleep 60
   end
 end
 
+
+#_,path,_ = line.split　これでもよし
 # break if line == "\r\n"
 # sock.gets&.chomp をつかうなら
 # break if line == ""
