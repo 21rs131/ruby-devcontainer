42c9f6cf7a30968d3cfc84c4f8b0b7b3f3e2fd38
Ryotaro MORIWAKI <taro@smkwlab.net>
2025-06-30 11:15:45 +0900

    Update client1.rb

--- src/client1.rb
+++ src/client1.rb
@@ -2,8 +2,14 @@
 
 require "socket"
 
-s = TCPSocket.open("www.is.kyusan-u.ac.jp", 80)
-s.print("GET / HTTP/1.0\r\n")
+path = ARGV
+#print(path)
+host = path[0]
+path = "/" + path[1] + "/"
+
+s = TCPSocket.open(host, 80)
+s.print("HEAD #{path} HTTP/1.1\r\n")
+s.print("Host: #{host}\r\n")
 s.print("\r\n")
 while line=s.gets&.chomp
   pp line
